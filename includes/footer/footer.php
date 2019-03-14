498354013e83bc938a85b5286304ce081c615df0
theo <t.polette@eleve.leschartreux.net>
2019-03-14 09:42:20 +0100

    ajout_includes

---
+++ includes/footer/footer.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pharmacie
+ * Date: 14/03/2019
+ * Time: 09:35
+ */
+?>
+
---
+++ includes/nav/nav.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pharmacie
+ * Date: 14/03/2019
+ * Time: 09:34
+ */
+?>
