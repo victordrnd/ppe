cc83cd180fb64ea5cbc0e3089a2bd7a43ae9a9af
Jules <jules.clear@yahoo.fr>
2019-03-14 19:29:30 +0100

    index

--- index.php
+++ index.php
@@ -4,7 +4,15 @@ include  $_SERVER['DOCUMENT_ROOT'].'/includes/header.php';
 
 ?>
 <body>
-
+  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+      <h1 class="display-4 font-weight-normal">Les nouveautées</h1>
+      <p class="lead font-weight-normal">StockPro, n°1 du high-tech et du matériel informatique, élu Service Client de l'Année. Comparez et achetez en livraison rapide à domicile.</p>
+      <a class="btn btn-outline-primary" href="#">Découvrez</a>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  </div>
 </body>
 
 <?php
