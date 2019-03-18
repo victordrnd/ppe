39c21663a489edf58103d694b6dbe5060053f606
theo <t.polette@eleve.leschartreux.net>
2019-03-18 21:34:32 +0100

    comencement de la page produits

---
+++ products/index.php
@@ -0,0 +1,100 @@
+<?php
+$root = '';
+include  $_SERVER['DOCUMENT_ROOT'].'/includes/header.php';
+
+?>
+<div  class="nav-link active color-dark h6 d-none d-md-block" style="text-align: center; margin-bottom: 50px; font-size: 2em;">
+    TOUS NOS PRODUITS
+</div>
+<div class="container-fluid">
+          <div class="row">
+
+                <div class=" col-sm-1 col-md-3" style="margin-bottom: 40px;">
+                    <div class="card" style="width: 18rem; text-align: center;">
+                        <img src="https://launches-media.endclothing.com/EE3709_launches_thumbnail.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Ajoutez au panier</a>
+                        </div>
+                    </div>
+                </div>
+              <div class=" col-sm-1 col-md-3" style="margin-bottom: 40px;">
+                  <div class="card" style="width: 18rem; text-align: center;">
+                      <img src="https://launches-media.endclothing.com/EE3709_launches_thumbnail.jpg" class="card-img-top" alt="">
+                      <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Ajoutez au panier</a>
+                      </div>
+                  </div>
+              </div>
+              <div class=" col-sm-1 col-md-3" style="margin-bottom: 40px;">
+                  <div class="card" style="width: 18rem; text-align: center;">
+                      <img src="https://launches-media.endclothing.com/EE3709_launches_thumbnail.jpg" class="card-img-top" alt="">
+                      <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Ajoutez au panier</a>
+                      </div>
+                  </div>
+              </div>
+              <div class=" col-sm-1 col-md-3" style="margin-bottom: 40px;">
+                  <div class="card" style="width: 18rem; text-align: center;">
+                      <img src="https://launches-media.endclothing.com/EE3709_launches_thumbnail.jpg" class="card-img-top" alt="">
+                      <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Ajoutez au panier</a>
+                      </div>
+                  </div>
+              </div>
+              <div class=" col-sm-1 col-md-3" style="margin-bottom: 40px;">
+                  <div class="card" style="width: 18rem; text-align: center;">
+                      <img src="https://launches-media.endclothing.com/EE3709_launches_thumbnail.jpg" class="card-img-top" alt="">
+                      <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Ajoutez au panier</a>
+                      </div>
+                  </div>
+              </div>
+              <div class=" col-sm-1 col-md-3" style="margin-bottom: 40px;">
+                  <div class="card" style="width: 18rem; text-align: center;">
+                      <img src="https://launches-media.endclothing.com/EE3709_launches_thumbnail.jpg" class="card-img-top" alt="">
+                      <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Ajoutez au panier</a>
+                      </div>
+                  </div>
+              </div>
+              <div class=" col-sm-1 col-md-3" style="margin-bottom: 40px;">
+                  <div class="card" style="width: 18rem; text-align: center;">
+                      <img src="https://launches-media.endclothing.com/EE3709_launches_thumbnail.jpg" class="card-img-top" alt="">
+                      <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Ajoutez au panier</a>
+                      </div>
+                  </div>
+              </div>
+              <div class=" col-sm-1 col-md-3" style="margin-bottom: 40px;">
+                  <div class="card" style="width: 18rem; text-align: center;">
+                      <img src="https://launches-media.endclothing.com/EE3709_launches_thumbnail.jpg" class="card-img-top" alt="">
+                      <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Ajoutez au panier</a>
+                      </div>
+                  </div>
+              </div>
+
+
+          </div>
+</div>
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/includes/footer/footer.php';
+?>
+
+
