cae913737f1fb665218a87366ad58eba380f60e3
CLEAR Jules <j.clear@eleve.leschartreux.net>
2019-03-28 09:47:11 +0100

    page prod

---
+++ products/details/index.php
@@ -0,0 +1,36 @@
+<?php
+include  $_SERVER['DOCUMENT_ROOT'].'/includes/header.php';
+?>
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb bg-white">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item"><a href="#">Library</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Data</li>
+  </ol>
+</nav>
+
+<div class="row">
+  <div class="col-md-6 col-12 d-flex">
+    <div class="card border-0 shadow small mt-3 ml-md-5 flex-fill">
+      <img class="w-75 d-block mx-auto" src="../../assets/index/gghome1.png" alt="">
+    </div>
+  </div>
+  <div class="col-md-6 col-12 d-flex">
+    <div class="card border-0 mx-5 flex-fill">
+      <h3 class="mt-3 text-center display-4">Nom produit</h3>
+      <h6 class="mt-3 ml-md-3 h2"><strong>999€</strong></h6>
+      <h6 class="mt-3 ml-md-3"><span class="badge badge-warning">En stock</span></h6>
+      <h6 class="mt-5 ml-md-3 font-weight-light"><span class="float-right mt-5">*Livraison gratuite à partir de 100€ d'achat</span></h6>
+      <a class="btn btn-warning float-right mx-3 mt-md-3" href="#">Achetez</a>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-12">
+    <div class="card border-0 mx-5 mt-5">
+      <h5 class="mt-3"><span class="float-right">Descriptif technique</span></h5>
+      <h6 class="mt-3 ml-md-3 font-weight-light text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h6>
+
+    </div>
+  </div>
+</div>
